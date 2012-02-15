1e9b49ba826790500c4c35841c61c58b59005620
Avijit Gupta <avgupta@login2.ls4.tacc.utexas.edu>
2012-02-15 01:54:48 -0600

    sample serial

--- job.sh
+++ job.sh
@@ -4,15 +4,13 @@
 #$ -cwd
 #$ -q development
 #$ -pe 12way 12
-#$ -N pmerge3
-#$ -o pmerge3.out
-#$ -e pmerge3.err
+#$ -N _3_10000
+#$ -o 3_10000.out
+#$ -e 3_10000.err
 #$ -M mailforavijit@gmail.com
 #$ -m be
 #$ -l h_rt=01:00:00
 
 export PATH=$PATH:$HOME/cilk/bin
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/cilk/lib
-cilkview 4_103
-cilkview 4_104
-cilkview 4_105
+cilkview 3_10000 -cilk_set_worker_count=1
